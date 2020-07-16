a5a589645776e6efaae50449f38a029f9483cc40
drakemain <demain1990@gmail.com>
2020-07-15 20:49:05 -0700

    title parsed from between quotes

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -4,13 +4,53 @@ import Command from './command';
 import Event from '../event';
 import { addEvent } from '../event-manager';
 
+const parseTitle = (params: string[]): string => {
+    let startIndex = -1;
+    let endIndex = -1;
+    let result: string = '';
+
+    for (let i = 0; i < params.length; ++i) {
+        const param = params[i];
+        if (startIndex < 0 && param.charAt(0) === '"') {
+            startIndex = i;
+        }
+
+        if (startIndex >= 0 && param.charAt(param.length - 1) == '"') {
+            endIndex = i;
+        }
+    }
+
+    if (endIndex >= 0) {
+        for (let i = startIndex; i <= endIndex; ++i) {
+            if (i === startIndex) {
+                result += params[i].substr(1);
+            } else if (i === endIndex) {
+                result += params[i].substr(0, params[i].length - 1);
+            } else {
+                result += params[i];
+            }
+
+            if (i < endIndex) {
+                result += ' ';
+            }
+        }
+    }
+
+    return result;
+};
+
 export default class extends Command {
     _name = 'create';
     _description = 'Creates a new event.'
 
     exec(params: string[], message: Message) {
         if (params.length >= 1) {
-            const title = params[0];
+            const title = parseTitle(params);
+
+            if (title === '') {
+                console.log('Cannot parse title');
+                return;
+            }
             let time = new Date();
             time.setSeconds(time.getSeconds() + 7);
 
